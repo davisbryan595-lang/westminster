f2297d46c4e1ed9fa04b729773734684e98f823a
Davis Bryan <davisbryan595@gmail.com>
2025-10-17 12:11:36 -0700

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -11,15 +11,16 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-20">
           <Link href="/" className="flex items-center gap-2">
-            <div className="w-12 h-12">
+            <div className="w-24 h-24">
               <img
                 src="/top-removebg-preview.png"
                 alt="Westminster Logo"
                 className="w-full h-full object-contain"
               />
             </div>
-            
+            <span className="hidden sm:inline font-bold text-lg text-foreground">Westminster</span>
           </Link>
+
 
 
           {/* Desktop Navigation */}
